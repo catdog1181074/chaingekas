74499145e444c115b97ebf5dd161acc64630bb30
catdog1181074 <kdoggo1@proton.me>
2025-05-30 15:08:34 -0400

    remove "vault"
    vault address removed, since only gets ~26% of its Kaspa inflow from known Chainge wallets as marked on kas.fyi. note that vault wallet received ~57M Kas directly from Finance Chaine wallet in Jan of 2024. Previous conclusions/results remain intact.

--- summary_chainge_to_cex_vs_threshold.py
+++ summary_chainge_to_cex_vs_threshold.py
@@ -13,7 +13,7 @@ THRESHOLDS = np.linspace(0.80, 0.9999, 21)
 # Known Chainge wallet addresses
 CHAINGE_ORIGINS = {
     "kaspa:qqwvnkp47wsj6n4hkdlgj8dsauyx0xvefunnwvvsmpq2udd0ka8ckmpuqw3k5",
-    "kaspa:qq9zagcza4jt76eev9jl5z0nqhe0thcu7js8larktj4sle7lvgnw7sfcewlty",
+    # "kaspa:qq9zagcza4jt76eev9jl5z0nqhe0thcu7js8larktj4sle7lvgnw7sfcewlty", # funded ~26% by Chainge, recieved ~57M Kas from Chainge Finance wallet
     "kaspa:qpgmt2dn8wcqf0436n0kueap7yx82n7raurlj6aqjc3t3wm9y5ssqtg9e4lsm",
     "kaspa:qpy03sxk3z22pacz2vkn2nrqeglvptugyqy54xal2skha6xh0cr7wjueueg79",
     "kaspa:qz9cqmddjppjyth8rngevfs767m5nvm0480nlgs5ve8d6aegv4g9xzu2tgg0u"
@@ -127,4 +127,3 @@ plt.ylim(50000000, 350000000)
 plt.grid(True)
 plt.gca().yaxis.set_major_formatter(ticker.FuncFormatter(lambda x, _: f'{int(x/1e6)}M'))
 plt.tight_layout()
-
