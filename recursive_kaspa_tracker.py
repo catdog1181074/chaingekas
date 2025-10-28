c8314693ceec031ba0670e38c8a5175bdcd1dc46
catdog <kdoggo1@proton.me>
2025-10-27 23:27:25 -0400

    Update recursive_kaspa_tracker.py
    restored vault after verifying its role in bridge transactions after Jan 27 2024

--- recursive_kaspa_tracker.py
+++ recursive_kaspa_tracker.py
@@ -148,7 +148,8 @@ CHAINGE_ROOTS = [
     "kaspa:qqwvnkp47wsj6n4hkdlgj8dsauyx0xvefunnwvvsmpq2udd0ka8ckmpuqw3k5",    
     "kaspa:qpgmt2dn8wcqf0436n0kueap7yx82n7raurlj6aqjc3t3wm9y5ssqtg9e4lsm",
     "kaspa:qpy03sxk3z22pacz2vkn2nrqeglvptugyqy54xal2skha6xh0cr7wjueueg79",
-    "kaspa:qz9cqmddjppjyth8rngevfs767m5nvm0480nlgs5ve8d6aegv4g9xzu2tgg0u"
+    "kaspa:qz9cqmddjppjyth8rngevfs767m5nvm0480nlgs5ve8d6aegv4g9xzu2tgg0u",
+    "kaspa:qq9zagcza4jt76eev9jl5z0nqhe0thcu7js8larktj4sle7lvgnw7sfcewlty"
 ]
 
 if __name__ == "__main__":
